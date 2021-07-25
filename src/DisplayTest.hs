b1623ceb94b413882771a16215223aef7b602711
dragoon8192_mac <dragoon8192@gmail.com>
2021-07-25 20:31:04 +0900

    update:   src/DisplayTest.hs

--- src/DisplayTest.hs
+++ src/DisplayTest.hs
@@ -2,11 +2,17 @@ module DisplayTest
     ( displayTest
     ) where
 import Graphics.Gloss
+import Data.Fixed
+import Data.List
 
 displayTest :: IO ()
-displayTest = animate window white $
-           \t -> circle $ 100 * t
-           -- translate (-150) (-10) . scale 0.5 0.5 $ text "Hello, World!"
+displayTest = animate window white animeCircle
    where
     window :: Display
-    window = InWindow "Hello, World!" (640,480) (100,100)
+    window = InWindow "Hello, World!" (640,480) (0,0)
+animeCircle :: Float -> Picture
+animeCircle t = pictures . map (circle . (*100)) . take 10 . iterate (+ tCycle) $ tMod
+  where
+    tCycle = 1.0
+    tMod = mod' t tCycle
+           -- translate (-150) (-10) . scale 0.5 0.5 $ text "Hello, World!"
