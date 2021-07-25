fefe9c9b4accc2822b3780646d87077f07813682
dragoon8192@yogabook <dragoon8192@gmail.com>
2021-07-25 18:52:27 +0900

    update:   src/DisplayTest.hs

--- src/DisplayTest.hs
+++ src/DisplayTest.hs
@@ -4,8 +4,9 @@ module DisplayTest
 import Graphics.Gloss
 
 displayTest :: IO ()
-displayTest = display window white $
-           translate (-150) (-10) . scale 0.5 0.5 $ text "Hello, World!"
-             where
-              window :: Display
-              window = InWindow "Hello, World!" (640,480) (100,100)
+displayTest = animate window white $
+           \t -> circle $ 100 * t
+           -- translate (-150) (-10) . scale 0.5 0.5 $ text "Hello, World!"
+   where
+    window :: Display
+    window = InWindow "Hello, World!" (640,480) (100,100)
